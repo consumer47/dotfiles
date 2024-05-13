257ce2d2b1141206989bf341ddbc8516b37ad86e
Dennis Schwichtenhoevel <dennis.schwichtenhoevel@rtb-bl.de>
2024-05-13 11:59:23 +0200

    i3 display to the right

---
+++ Contextor/contextor/contextor.py
@@ -0,0 +1,22 @@
+# Contextor/contextor/contextor.py
+
+class Contextor:
+    def __init__(self):
+        # Initialize your contextor
+        pass
+
+    def stage_files(self, files):
+        # Logic to stage files
+        pass
+
+    def set_context(self, context_name):
+        # Logic to switch contexts
+        pass
+
+    def output_tree(self):
+        # Logic to output the project tree
+        pass
+
+if __name__ == "__main__":
+    # Command-line interface logic
+    pass
