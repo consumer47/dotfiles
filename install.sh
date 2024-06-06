d55ada2aff658488e91ac10649f9bbbb15d35329
Dennis Schwichtenhoevel <dennis.schwichtenhoevel@rtb-bl.de>
2024-06-06 16:21:18 +0200

    ...

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Install essential packages
+install_packages() {
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        sudo apt-get update
+        sudo apt-get install -y git stow zsh neovim
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        brew update
+        brew install git stow zsh neovim
+    fi
+}
+
+# Clone dotfiles repository
+clone_dotfiles() {
+    git clone https://github.com/consumer47/dotfiles.git ~/dotfiles
+    cd ~/dotfiles
+}
+
+# Deploy dotfiles using GNU Stow
+deploy_dotfiles() {
+    cd ~/dotfiles
+    stow -v zsh
+    stow -v nvim
+}
+
+# Main function to orchestrate the setup
+main() {
+    install_packages
+    clone_dotfiles
+    deploy_dotfiles
+    echo "Installation complete!"
+}
+
+main
+
